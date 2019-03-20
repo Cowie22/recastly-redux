0e69dba9285706616df8ecdd76e32b2c1bc06cc7
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-03-20 15:05:23 -0700

    Adds containers

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,9 +2,22 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+
+const mapStateToProps = (state) => {
+  return {
+    videos: state.videoList,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleVideoListEntryTitleClick: (videos) => {
+      dispatch(changeVideo(videos));
+    }
+  };
+};
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
 
-export default VideoListContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(VideoList);
--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,9 +1,22 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
+
+const mapStateToProps = (state) => {
+  return {
+    video: state.currentVideo,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleVideoListEntryTitleClick: (videos) => {
+      dispatch(changeVideo(videos));
+    }
+  };
+};
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
 
-export default VideoPlayerContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(VideoPlayer);
